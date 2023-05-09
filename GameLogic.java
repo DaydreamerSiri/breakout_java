b9ff84c310c3c5349d7ac5a29ad5476aa2cf3efc
tjaja <tjajansen@web.de>
2023-05-09 16:04:12 +0200

    GameLogic erstellt

---
+++ GameLogic.java
@@ -0,0 +1,19 @@
+/**
+ *Klasse um die Anzahl der Bälle, sowie den Spielscore wiedergibt
+ * @Author Tjark Jansen
+ * @Author Sehri Singh
+ */
+public class GameLogic {
+    private int ballCount;
+    private int score;
+
+    /**
+     * Konstruktor Methode
+     * @param ballCount Anzahl der noch verfügbaren Bälle
+     * @param score Anzahl der verdienten Punkte
+     */
+    public GameLogic(int ballCount, int score){
+        this.ballCount = ballCount;
+        this.score = score;
+    }
+}
